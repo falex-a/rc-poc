ad98db1e90a0e284cd10c5734767d2932f89336a
AlexF <alexf@medaware.com>
2017-04-01 23:03:24 +0300

    remembering the active controllers and switching between them each 10sec. Seems to work, haha

--- rc.js
+++ rc.js
@@ -39,38 +39,77 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, 'html')));
 
 var dgram = require('dgram');
-var PORT = 1234;
-var HOST = 'none'; //192.168.2.109';
+var DEVICE_PORT = 1234;
+var DEVICE_IP = 'NA'; //192.168.2.109';
 var client = dgram.createSocket('udp4');
 
-// Add REST interface to get coordinates by AJAX from virtualjoystick.js..
-//   ...and act as a broker, relaying x,y to the IoT device via UDP
-app.get('/xy', function(req, res) {
-    console.log('x,y: '+req.query.x+','+req.query.y);
-    var message = new Buffer(req.query.x+','+req.query.y);
-    // TODO - optimize, maybe run a queue in a separate thread..
-    if( HOST!=='none') {
-        client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-            if (err) throw err;
-            console.log('UDP message sent to ' + HOST +':'+ PORT);
-            // client.close();
-        });
-    }
+// initialize the instanteneous Vx,Vy commands
+var commVX=0;
+var commVY=0;
 
+var hosts = [];
+var host_cmds = {};
+
+// Add REST interface to get coordinates by AJAX from virtualjoystick.js..
+//   ...and act as a broker, relaying x,y (..received as URL parameters..) to the IoT device via UDP
+app.get('/xy', function(req, res) {
+
+    host = req.connection.remoteAddress.toString();
+    if( hosts.indexOf(host)===-1 ) {
+        hosts.push(host);
+        console.log('New controller, IP='+host);
+        console.log('curr controllers IPs list: '+hosts);
+    }    
+    host_cmds[host] = [req.query.x, req.query.y]; 
+    if( !(req.query.x==='0' && req.query.y==='0') ) {        
+        console.log('command from '+host+': x,y = '+req.query.x+','+req.query.y);  
+    }
+    commVX = req.query.x;
+    commVY = req.query.y;
     res.send('OK');
+    //console.log(host_cmds);
    // res.send('{"id": 1,"name":"Matt","band":"BBQ Brawlers"}');
 });
 
 app.listen(3001);
 console.log('Listening on port 3001, try <ip>:3001/html/joy.html');
 
-// OK, now let's try to listen to the nodeMCU identify itself...
+// OK, now let's  try to listen to the nodeMCU identify itself...
 udpsock = dgram.createSocket('udp4');
 udpsock.on('message', function (msg, info){
     console.log("got UDP msg: "+msg.toString());
-    HOST = msg.toString();
+    DEVICE_IP = msg.toString();
  });
 
 udpsock.bind(6666);
-console.log('Listening for UDP on port 6666...');
+console.log('Listening for self-discovery UDP on port 6666...');
 
+var i=0;
+var ctrlInd = 0;
+
+// Each 50ms send UDP update to IoT device
+setInterval(function(){
+    // console.log('test');
+    if( ! hosts.length ) {
+        return;
+    }
+    if(i++ > 200) { // switch controller :)
+        ctrlInd = (ctrlInd+1) % hosts.length;
+        if(hosts.length>1) {
+            console.log("Switched active controller to "+hosts[ctrlInd]);
+        }
+        i=0;
+    }
+    //console.log('tick tock');    
+    var actCtrlVxy = host_cmds[hosts[ctrlInd]];
+    // var message = new Buffer(commVX+','+commVY);
+    var message = new Buffer(actCtrlVxy[0]+','+actCtrlVxy[1]);
+    // console.log(actCtrlVxy[0]+','+actCtrlVxy[1]);
+    if( DEVICE_IP!=='NA' ) { // && !(commVX===0 && commVY===0) ) {        
+        client.send(message, 0, message.length, DEVICE_PORT, DEVICE_IP, function(err, bytes) {
+            if (err) throw err;
+            // console.log('UDP message sent to ' + DEVICE_IP +':'+ DEVICE_PORT);
+            // client.close();
+        });
+    }
+}, 50);
